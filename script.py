59404e077a0aadb0b0a5998e9db63b96e097ecc0
idkgj1233 <79495644+idkgj1233@users.noreply.github.com>
2021-04-03 19:44:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,10 +2,10 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-I'm a Image Editor Bot which Supports various modes
+I am <b>🌈Saviour Coders' Image Editor Bot🌈</b> which Supports various editing modes.
 
-For more click help....</b>"""
-    HELP_MSG = """Hai, Follow these Steps..
+For more <b>lick👉 /help</b>"""
+    HELP_MSG = """Hi, Follow these Steps..
 
 <code>🌀 Send me any Image to Edit..</code>
 
@@ -13,12 +13,14 @@ For more click help....</b>"""
 
 <code>🌀 Your Edited Image will be Uploaded </code>
 
-
-© @TroJanzHEX"""
+<b>Support Group</b> :- 🐠@ubuntu_coders🐠
+<b>Support Channel</b> :- 🐬@UC_bot_channel🐬
+<b>My Master</b> :- ⛵️@saviour_coder⛵️"""
+    
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/idkgj1233/SC-Image-Editor-Bot'>Click Here</a>"""
